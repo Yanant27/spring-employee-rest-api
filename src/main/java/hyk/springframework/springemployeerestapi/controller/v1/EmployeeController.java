4322e7f595baccbc509f2a0620782e15bf1ba72c
Yanant27 <htooyanant.hyk@gmail.com>
2022-11-08 19:50:27 +0630

    Add Springdoc-OpenAPI.

--- src/main/java/hyk/springframework/springemployeerestapi/controller/v1/EmployeeController.java
+++ src/main/java/hyk/springframework/springemployeerestapi/controller/v1/EmployeeController.java
@@ -24,7 +24,7 @@ public class EmployeeController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public EmployeeListDTO getListOfEmployees()  {
+    public EmployeeListDTO getListOfEmployees() {
         return new EmployeeListDTO(employeeService.getAllEmployees());
     }
 
